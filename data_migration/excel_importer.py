5edd4b2c06513eb82eba744a6ff11908cd08817f
Marc Lambrecht <mla@enda.eu>
2022-11-02 00:38:22 +0100

    import all games

--- data_migration/excel_importer.py
+++ data_migration/excel_importer.py
@@ -3,18 +3,18 @@ import pandas
 class ExcelImporter:
     """ Read and export the kennerliga excel file to the django database """
 
-    def __init__(self, file_name='data.xlsm', year=2022):
-        self.file_name = file_name
-        self.year = year
+    def __init__(self):
         self.df = self.read_file()
+        self.year = 2022
         self.bga_names = set()
         self.data = {
             'games':{},
             'bga_names':set(),
+            'results':[],
         }
 
-    def read_file(self):
-        self.df = pandas.read_excel(self.file_name, None)
+    def read_file(self, file_name='data.xlsm'):
+        self.df = pandas.read_excel(file_name, None)
         return self.df
 
     def get_sheet(self, number=1):
@@ -28,19 +28,22 @@ class ExcelImporter:
     def get_cell(self, sheet, row, col):
         return sheet.iloc[row][col]
 
-    def get_keyword_locations(self, month,  keywords):
+    def get_keyword_locations(self, month,  keywords, exact=False):
         locations = []
         sheet = self.get_sheet(month)
         rows, cols = sheet.shape
         for col in range(cols):
             for row in range(rows):
+                cell = self.get_cell(sheet, row, col)
+                if (isinstance(cell, float)) or isinstance(cell, int):
+                    continue
                 for keyword in keywords:
-                    cell = self.get_cell(sheet, row, col)
-                    if (isinstance(cell, float)) or isinstance(cell, int):
-                        continue
+                    if exact:
+                        condition = keyword == cell
                     else:
-                        if keyword in cell:
-                            locations.append({"keyword":keyword, "row": row, "col": col})
+                        condition = keyword in cell
+                    if condition:
+                        locations.append({"keyword":keyword, "row": row, "col": col})
         return locations
 
     def get_bga_names_from_gesamtwertung(self):
@@ -51,7 +54,7 @@ class ExcelImporter:
         col =  3
         for row in range(start_row, end_row):
             cell =  self.get_cell(sheet, row, col)
-            if pandas.isna(cell):
+            if self.cell_isna(cell):
                 return
             else:
                 self.data['bga_names'].add(cell)
@@ -113,14 +116,59 @@ class ExcelImporter:
                 game = self.get_cell(sheet, row, game_col)
                 option = self.get_cell(sheet, row, option_col)
 
-
     def get_match_results(self, month):
-        pass
+        keywords = ['Platzierung']
+        locations = self.get_keyword_locations(month, keywords, True)
+        for location in locations:
+            self.store_match_results(location, month)
+
+    def store_match_results(self, location, month):
+        sheet = self.get_sheet(month)
+        row = location['row']
+        game = self.get_cell(sheet, row-1, 4)
+        next_row = row + 1
+        while self.store_match_result(next_row, month, game, sheet):
+            self.store_match_result(next_row, month, game, sheet)
+            next_row += 1
+
+    def store_match_result(self, row, month, game, sheet):
+        player = self.get_cell(sheet, row, 5)
+        if self.cell_isna(player):
+            return False
+        starting_position = self.get_cell(sheet, row, 6)
+        character = self.get_cell(sheet, row, 7)
+        starting_points = self.get_cell(sheet, row, 8)
+        final_points = self.get_cell(sheet, row, 9)
+        tie_breaker = self.get_cell(sheet, row, 10)
+        percentage_of_winner = self.get_cell(sheet, row, 11)
+        position = self.get_cell(sheet, row, 12)
+        league_points = self.get_cell(sheet, row, 13)
+        year = self.year
+        season = month
+        result = {
+                "player":player,
+                "starting_position":starting_position,
+                "character":character,
+                "starting_points":starting_points,
+                "final_points":final_points,
+                "tie_breaker":tie_breaker,
+                "percentage_of_winner":percentage_of_winner,
+                "position":position,
+                "league_points":league_points,
+                "year":year,
+                "season":season,
+                "game":game,
+         }
+        self.data['results'].append(result)
+
+
+
+
 
 I = ExcelImporter()
 I.read_file()
 I.get_bga_names_from_gesamtwertung()
-I.iterate_all_sheets(I.get_games_from_leagues)
+I.iterate_all_sheets(I.get_games_from_leagues, I.get_match_results)
 print(I.data)
 
 # def get_bga_names_from_anmeldung(self, month):
