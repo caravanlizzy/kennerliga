f2e530373d7cdd52c3efbe1c3cf08a2a5be58914
Marc Lambrecht <mla@enda.eu>
2022-11-05 14:19:23 +0100

    correct excel typos, add minor exceptions for 2021 and 2022 document structure. 2021 2022 import final

--- data_migration/excel_importer.py
+++ data_migration/excel_importer.py
@@ -4,8 +4,7 @@ class ExcelImporter:
     """ Read and export the kennerliga excel file to the django database """
 
     def __init__(self):
-        self.df = self.read_file()
-        self.year = 2022
+        self.df = {} # store dataframes with year as their key
         self.bga_names = set()
         self.data = {
             'games':{},
@@ -13,13 +12,17 @@ class ExcelImporter:
             'results':[],
         }
 
-    def read_file(self, file_name='data.xlsm'):
-        self.df = pandas.read_excel(file_name, None)
-        return self.df
+    def read_file(self, year):
+        file_end = '.xlsm'
+        file_name = str(year) + file_end
+        self.df[year] = pandas.read_excel(file_name, None)
 
-    def get_sheet(self, number=1):
-        season_key = f'{self.year}_S{number}'
-        return self.df[season_key]
+    def get_file(self, year):
+        return self.df[year]
+
+    def get_sheet(self, year, number=1):
+        season_key = f'{year}_S{number}'
+        return self.get_file(year)[season_key]
 
     def cell_isna(self, cell):
         return pandas.isna(cell)
@@ -28,9 +31,9 @@ class ExcelImporter:
     def get_cell(self, sheet, row, col):
         return sheet.iloc[row][col]
 
-    def get_keyword_locations(self, month,  keywords, exact=False):
+    def get_keyword_locations(self, year,  month,  keywords, exact=False):
         locations = []
-        sheet = self.get_sheet(month)
+        sheet = self.get_sheet(year, month)
         rows, cols = sheet.shape
         for col in range(cols):
             for row in range(rows):
@@ -46,9 +49,9 @@ class ExcelImporter:
                         locations.append({"keyword":keyword, "row": row, "col": col})
         return locations
 
-    def get_bga_names_from_gesamtwertung(self):
-        sheet_key = f'{self.year}_GESAMTWERTUNG'
-        sheet = self.df[sheet_key]
+    def get_bga_names_from_gesamtwertung(self, year):
+        sheet_key = f'{year}_GESAMTWERTUNG'
+        sheet = self.get_file(year)[sheet_key]
         start_row = 4
         end_row = sheet.shape[0];
         col =  3
@@ -59,19 +62,20 @@ class ExcelImporter:
             else:
                 self.data['bga_names'].add(cell)
 
-    def iterate_all_cells(self, sheet_number,  callback):
-        sheet = self.get_sheet(sheet_number)
+    def iterate_all_cells(self, year, sheet_number, callback):
+        sheet = self.get_sheet(year, sheet_number)
         rows, cols = sheet.shape
         for row in range(rows):
             for col in range(cols):
                 cell = self.get_cell(sheet, row, col)
                 callback(cell, row, col)
 
-    def iterate_all_sheets(self, *callbacks):
-        for month in range(1, len(self.df) -1 ): # dont consider last 2 sheets, gesamtwertung and definitonen
+    def iterate_all_sheets(self, year, *callbacks):
+        file = self.get_file(year)
+        for month in range(1, len(file) -1 ): # dont consider last 2 sheets, gesamtwertung and definitonen
             print(f'season: {month}')
             for callback in callbacks:
-                callback(month)
+                callback(year, month)
 
     def generate_liga_keywords(self, max_ligen):
         keywords = []
@@ -79,9 +83,9 @@ class ExcelImporter:
             keywords.append(f'Liga {liga}')
         return keywords
 
-    def get_games_locations(self, month):
+    def get_games_locations(self, year, month):
         keywords = self.generate_liga_keywords(5)
-        locations = self.get_keyword_locations(month, keywords)
+        locations = self.get_keyword_locations(year, month, keywords)
         return locations
 
     def store_game(self, game):
@@ -92,6 +96,7 @@ class ExcelImporter:
         if options == '-':
             return
         options = options.split('\n')
+        print(options)
         for option in options:
             option = option[2:]
             option, value = option.split(':')
@@ -100,15 +105,26 @@ class ExcelImporter:
 
 
 
-    def get_games_from_leagues(self, month):
-        sheet = self.get_sheet(month)
-        for location in self.get_games_locations(month):
-            start_row = location['row'] + 4
-            game_col = location['col'] + 5
+    def get_games_from_leagues(self, year,  month):
+        sheet = self.get_sheet(year, month)
+        rows_down_to_value = 4
+        cols_right_to_value = 5
+        if year == 2020:
+            cols_right_to_value -= 1
+            rows_down_to_value += 1
+            if month > 1:
+                cols_right_to_value += 1
+        for location in self.get_games_locations(year, month):
+            start_row = location['row'] + rows_down_to_value
+            game_col = location['col'] + cols_right_to_value
             option_col = game_col + 1
             row = start_row
-            game = self.get_cell(sheet, row, game_col)
-            option = self.get_cell(sheet, row, option_col)
+            try:
+                game = self.get_cell(sheet, row, game_col)
+                option = self.get_cell(sheet, row, option_col)
+            except IndexError:
+                print('index error - is caught: if year not 2021, check in detail')
+            print(game, option)
             while not self.cell_isna(game):
                 self.store_game(game)
                 self.store_game_option(game, option)
@@ -116,22 +132,22 @@ class ExcelImporter:
                 game = self.get_cell(sheet, row, game_col)
                 option = self.get_cell(sheet, row, option_col)
 
-    def get_match_results(self, month):
+    def get_match_results(self, year, month):
         keywords = ['Platzierung']
-        locations = self.get_keyword_locations(month, keywords, True)
+        locations = self.get_keyword_locations(year, month, keywords, True)
         for location in locations:
-            self.store_match_results(location, month)
+            self.store_match_results(location, year,  month)
 
-    def store_match_results(self, location, month):
-        sheet = self.get_sheet(month)
+    def store_match_results(self, location, year, month):
+        sheet = self.get_sheet(year, month)
         row = location['row']
         game = self.get_cell(sheet, row-1, 4)
         next_row = row + 1
-        while self.store_match_result(next_row, month, game, sheet):
+        while self.store_match_result(next_row, year, month, game, sheet):
             self.store_match_result(next_row, month, game, sheet)
             next_row += 1
 
-    def store_match_result(self, row, month, game, sheet):
+    def store_match_result(self, row, year, month, game, sheet):
         player = self.get_cell(sheet, row, 5)
         if self.cell_isna(player):
             return False
@@ -143,7 +159,7 @@ class ExcelImporter:
         percentage_of_winner = self.get_cell(sheet, row, 11)
         position = self.get_cell(sheet, row, 12)
         league_points = self.get_cell(sheet, row, 13)
-        year = self.year
+        year = year
         season = month
         result = {
                 "player":player,
@@ -166,9 +182,11 @@ class ExcelImporter:
 
 
 I = ExcelImporter()
-I.read_file()
-I.get_bga_names_from_gesamtwertung()
-I.iterate_all_sheets(I.get_games_from_leagues, I.get_match_results)
+years = [2021, 2022]
+for year in years:
+    I.read_file(year)
+    I.get_bga_names_from_gesamtwertung(year)
+    I.iterate_all_sheets(year, I.get_games_from_leagues, I.get_match_results)
 print(I.data)
 
 # def get_bga_names_from_anmeldung(self, month):
