d14aae075dde9310b232d2d2db7212f971322b23
Marc Lambrecht <mla@enda.eu>
2022-11-20 16:21:37 +0100

    import user data

--- kennerliga/settings.py
+++ kennerliga/settings.py
@@ -132,7 +132,6 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
 
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
