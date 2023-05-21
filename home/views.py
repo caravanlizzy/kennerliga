74270189428a9481d2648723f1edab1813d23421
Marc Lambrecht <mla@enda.eu>
2023-05-21 12:35:53 +0200

    add announcement area

--- home/views.py
+++ home/views.py
@@ -2,6 +2,15 @@
 # Create your views here.
 from django.views.generic import TemplateView
 
+from season.models import Season
+
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        open_season = Season.objects.filter(status=Season.SeasonStatus.OPEN)
+        if open_season:
+            context['open_season'] = open_season
+        return context
