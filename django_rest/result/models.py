d4e7d7ec31ca826cb795864fedf97144640413a8
Marc Kittner <m.kittner@kunbus.com>
2024-01-03 18:19:14 +0100

    archive django_app, create django_rest api

---
+++ django_rest/result/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from user.models import User
+from season.models import Season
+
+
+# Create your models here.
+class MatchResult(models.Model):
+    player = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    position = models.IntegerField()
+    points = models.IntegerField()
+    league = models.IntegerField()
+    season = models.ForeignKey(
+        Season,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    year = models.IntegerField()
+    starting_position = models.IntegerField(blank=True, null=True)
+    starting_points = models.IntegerField(blank=True, null=True)
+    league_points = models.IntegerField()
+    percentage_of_winner = models.FloatField(blank=True, null=True)
+    character = models.CharField(
+        max_length=144,
+        blank=True,
+        null=True
+    )
+    tie_breaker = models.CharField(
+        max_length=144,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.player.username + '_' + str(self.year) + '_' + 'S' + str(self.season) + '_' + 'L' + str(self.league)
---
+++ django_rest/season/models.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+from user.models import User
+
+
+# Create your models here.
+class Season(models.Model):
+    class SeasonStatus(models.TextChoices):
+        NEXT = 'Nächste'
+        OPEN = 'Anmeldung offen'
+        RUNNING = 'Laufend'
+        DONE = 'Beendet'
+
+    participants = models.ManyToManyField(
+        User,
+        blank=True,
+        related_name='seasons_participating'
+    )
+    year = models.IntegerField()
+    month = models.IntegerField(
+        'month in the current year',
+        validators=[MinValueValidator(1), MaxValueValidator(12)]
+    )
+    status = models.CharField(
+        max_length=15,
+        choices=SeasonStatus.choices,
+        default=SeasonStatus.NEXT
+    )
+
+    @property
+    def season_start_time(self):
+        return datetime(self.year, self.month, 1, 0, 0)
+
+    @property
+    def time_to_start(self):
+        return self.season_start_time - datetime.now()
+
+    def __str__(self):
+        return f'{self.year}_S{self.month}'
+
+    def create_leagues(self):
+        pass
+
+    def get_player_order(self, season_number):
+        participants = self.objects.filter(number=season_number).participants.all()
+
+    @staticmethod
+    def get_players_per_league(self, participants_amount):
+        rest_players = participants_amount % 4
+        league_amount = participants_amount // 4 + 1 if rest_players > 0 else participants_amount // 4
+        for league in range(league_amount):
+            players_per_league = league_amount * [4]
+        if rest_players == 1:
+            players_per_league[-1] = 2
+            players_per_league[-2] = 3
+        elif rest_players == 2:
+            players_per_league[-1] = 2
+        elif rest_players == 3:
+            players_per_league[-1] = 3
+        return players_per_league
+
+# class Registration(models.Model):
+#     player = models.ManyToManyField(
+#         User,
+#     )
+#     season = models.ForeignKey(
+#         Season,
+#         on_delete=models.CASCADE,
+#     )
+#     datetime = models.DateTimeField(auto_now_add=True, editable=False)
