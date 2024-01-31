e7654385d92a49c6300f325f478274a885a17320
Marc Kittner <m.kittner@kunbus.com>
2024-01-31 16:01:56 +0100

    fix: Try static dirs againagain

--- api/django_rest/settings.py
+++ api/django_rest/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'dist/spa'),
-    os.path.join(BASE_DIR, 'dist/spa/assets'),
+#     os.path.join(BASE_DIR, 'dist/spa/assets'),
 ]
 
 # Default primary key field type
