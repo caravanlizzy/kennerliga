a59bc53b94780c254cd12aaed41968d1e982a36e
Marc Kittner <m.kittner@kunbus.com>
2024-01-31 15:30:14 +0100

    fix: Adjust static dist folder

--- api/django_rest/settings.py
+++ api/django_rest/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/dist')
+    os.path.join(BASE_DIR, '../frontend/dist/spa')
 ]
 
 # Default primary key field type
