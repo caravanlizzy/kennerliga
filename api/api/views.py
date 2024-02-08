2d28cab75aa3c1ac51e63d868d77280f72643c67
caravanlizzy <marc.lambrecht@gmx.de>
2024-02-08 22:15:04 +0100

    get token on login

--- api/api/views.py
+++ api/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
 
 class LoginApiView(APIView):
     def post(self, request, *args, **kwargs):
@@ -19,17 +20,34 @@ class LoginApiView(APIView):
         if user is not None:
             # If authentication successful, return user data or token
             # For example, you can return user data or JWT token here
+            print(get_token(user))
             return Response({'message': 'Login successful' , "user": get_user_information(user)}, status=status.HTTP_200_OK)
         else:
             # If authentication failed, return error message
             return Response({'error': 'Invalid username or password'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
+def create_token(user):
+    return Token.objects.create(user=user)
+
+def has_token(user):
+    return Token.objects.filter(user=user).exists()
+
+def get_token(user):
+    if has_token(user):
+        token = Token.objects.get(user=user)
+    else:
+        token = create_token(user)
+    return str(token)
+
+
 def get_user_information(user):
+    token = get_token(user)
     user = {
         "id": user.id,
         "username": user.username,
         "bga_name": user.bga_name,
         "email": user.email,
+        "token": token,
     }
     return user
