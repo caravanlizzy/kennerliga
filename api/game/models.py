d0f8da80f2eca2d5d077608fed2ff3794592c305
Marc Kittner <m.kittner@kunbus.com>
2024-04-02 21:38:03 +0200

    add SelectedGame to store a game election

--- api/game/models.py
+++ api/game/models.py
@@ -45,3 +45,24 @@ class GameOptionChoice(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+
+class SelectedGame(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    option = models.ForeignKey(GameOption, on_delete=models.CASCADE)
+    boolean_value = models.BooleanField(null=True, blank=True)
+    choice_value = models.ForeignKey(GameOptionChoice, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.game.name} - {self.option.name}"
+
+    @classmethod
+    def create_selected_game(cls, game, option, boolean_value=None, choice_value=None):
+        selected_game = cls(game=game, option=option)
+        if option.has_choices:
+            selected_game.choice_value = choice_value
+        else:
+            selected_game.boolean_value = boolean_value
+        selected_game.save()
+        return selected_game
