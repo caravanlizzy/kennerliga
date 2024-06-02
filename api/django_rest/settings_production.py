920e07245461b9d475876f260fa5cd4f5e87489e
Marc Kittner <m.kittner@kunbus.com>
2024-06-02 22:03:09 +0200

    chore: Add production database settings

---
+++ api/django_rest/settings_production.py
@@ -0,0 +1,11 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'haligh$kennerliga',
+        'USER': 'haligh',
+        'PASSWORD': 'kennerdatabasessssssss1117',
+        'HOST': 'haligh.mysql.pythonanywhere-services.com',
+    }
+}
