4afde2775eb7ca43de92903ed472db87c148af27
Marc Kittner <m.kittner@kunbus.com>
2024-06-02 22:31:56 +0200

    chore: split dev settings

--- api/django_rest/settings.py
+++ api/django_rest/settings.py
@@ -97,13 +97,6 @@ WSGI_APPLICATION = 'django_rest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
