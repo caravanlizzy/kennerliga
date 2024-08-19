55dba86116c2d0a37b5cf78579e38ca6e4bb1860
Marc Kittner <m.kittner@kunbus.com>
2024-08-19 10:05:32 +0200

    add create saeson

--- api/season/models.py
+++ api/season/models.py
@@ -62,6 +62,29 @@ class Season(models.Model):
             players_per_league[-1] = 3
         return players_per_league
 
+
+def get_current_season():
+    return Season.objects.order_by('-year', '-month').first()
+
+
+def get_new_month_year(month, year):
+    new_month = month + 1
+    new_year = year
+    if new_month == 13:
+        new_year += 1
+        new_month = 1
+    return [new_month, new_year]
+
+
+def create_season():
+    current_season = get_current_season()
+    if not current_season:
+        print("No seasons created so far.")
+        return
+    new_month, new_year = get_new_month_year(current_season.month, current_season.year)
+    new_season = Season(year=new_year, month=new_month)
+    new_season.save()
+
 # class Registration(models.Model):
 #     player = models.ManyToManyField(
 #         User,
