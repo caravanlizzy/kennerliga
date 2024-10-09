6f3edd85b0af465c4f4d06473919f68a6e8c7f2c
Marc Kittner <marc.lambrecht@gmx.de>
2024-10-09 11:35:27 +0200

    feat: add endpoints for users my_leages, my_seasons, my_results

--- api/user/views.py
+++ api/user/views.py
@@ -1,6 +1,14 @@
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from league.models import League
+from league.serializer import LeagueSerializer
+from result.models import Result
+from result.serializers import ResultSerializer
+from season.models import Season
+from season.serializer import SeasonSerializer
 from user.models import User
 from user.serializers import UserSerializer
 
@@ -10,3 +18,33 @@ class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+    @action(detail=True, methods=['get'], url_path='leagues')
+    def user_leagues(self, request, pk=None):
+        """
+        Custom action to return the leagues for a specific user.
+        """
+        user = self.get_object()  # Fetch the user by the ID from the URL (pk)
+        leagues = League.objects.filter(members=user.profile)
+        serializer = LeagueSerializer(leagues, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'], url_path='seasons')
+    def user_seasons(self, request, pk=None):
+        """
+        Custom action to return the leagues for a specific user.
+        """
+        user = self.get_object()  # Fetch the user by the ID from the URL (pk)
+        season = Season.objects.filter(participants=user.profile)
+        serializer = SeasonSerializer(season, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'], url_path='results')
+    def user_results(self, request, pk=None):
+        """
+        Custom action to return the leagues for a specific user.
+        """
+        user = self.get_object()  # Fetch the user by the ID from the URL (pk)
+        results = Result.objects.filter(player_profile=user.profile)
+        serializer = ResultSerializer(results, many=True)
+        return Response(serializer.data)
