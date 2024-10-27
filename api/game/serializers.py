863a007cc6b205f3ac98c3efa71749771df2704c
Marc Kittner <marc.lambrecht@gmx.de>
2024-10-27 16:41:11 +0100

    feat: create well structured SelectedGame serializer

--- api/game/serializers.py
+++ api/game/serializers.py
@@ -52,10 +52,49 @@ class PlatformSerializer(ModelSerializer):
         fields = '__all__'
 
 
-class SelectedGameSerializer(ModelSerializer):
+from rest_framework import serializers
+from .models import SelectedGame, SelectedOption
+
+
+class SelectedOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SelectedOption
+        fields = ['id', 'game_option', 'choice', 'value']
+
+
+class SelectedGameSerializer(serializers.ModelSerializer):
+    selected_options = SelectedOptionSerializer(many=True)
+
     class Meta:
         model = SelectedGame
-        fields = '__all__'
+        fields = ['id', 'player', 'game', 'league', 'selected_options']
+
+    def create(self, validated_data):
+        selected_options_data = validated_data.pop('selected_options')
+        selected_game = SelectedGame.objects.create(**validated_data)
+        for option_data in selected_options_data:
+            SelectedOption.objects.create(selected_game=selected_game, **option_data)
+        return selected_game
+
+    def update(self, instance, validated_data):
+        selected_options_data = validated_data.pop('selected_options', None)
+        instance.player = validated_data.get('player', instance.player)
+        instance.game = validated_data.get('game', instance.game)
+        instance.league = validated_data.get('league', instance.league)
+        instance.save()
+
+        if selected_options_data:
+            for option_data in selected_options_data:
+                option_id = option_data.get('id')
+                if option_id:
+                    option_instance = SelectedOption.objects.get(id=option_id, selected_game=instance)
+                    option_instance.choice = option_data.get('choice', option_instance.choice)
+                    option_instance.value = option_data.get('value', option_instance.value)
+                    option_instance.save()
+                else:
+                    SelectedOption.objects.create(selected_game=instance, **option_data)
+
+        return instance
 
 
 class SelectedOptionSerializer(ModelSerializer):
