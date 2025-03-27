ccff56a75a7d1a1c9c03d123dfc4d299aa629c54
Marc Kittner <marc.lambrecht@gmx.de>
2025-03-27 12:36:00 +0100

    update production API_URL
    Since kennerliga.de is temp off (pricing for custom URL), back to
    default url.

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -58,7 +58,7 @@ module.exports = configure(function (ctx) {
       env: {
         API_URL: ctx.dev
           ? 'http://localhost:8000/api'
-          : 'https://www.kennerliga.de/api'
+          : 'www.haligh.pythonanywhere.com/api'
       },
       target: {
         browser: [ 'es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1' ],
