98f3be080f2a1adc9926e079a6bd318bab10062a
Marc Kittner <marc.kittner@posteo.de>
2025-04-12 18:22:36 +0200

    temporarily add sessionAuth to use APi UI

--- api/django_rest/settings.py
+++ api/django_rest/settings.py
@@ -89,6 +89,7 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # ← Add this TODO: REMOVE BEFORE DEPLOY
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
