5523708c74637dd1832f9a5360b83826cc8301af
Marc Kittner <m.kittner@kunbus.com>
2025-09-09 17:46:33 +0200

    feat: control ContentSection visibility
    Signed-off-by: Marc Kittner <m.kittner@kunbus.com>

--- frontend/src/pages/MyLeague.vue
+++ frontend/src/pages/MyLeague.vue
@@ -3,18 +3,25 @@
     <LeagueStatusBar />
     <!-- Game Selector - shown when user needs to pick games -->
     <template v-if="isMePickingGame">
-      <ContentSection title="Game Selection" color="positive">
+      <ContentSection
+        title="Game Selection"
+        color="positive"
+        :is-opened="sectionVisibilityStates['selection']"
+      >
         <GameSelector
           @submit-success="updateLeagueData"
           class="q-mt-md q-pa-xs"
         />
       </ContentSection>
-
     </template>
 
     <!-- Match Results Section -->
     <template v-if="leagueStatus === 'PLAYING'">
-      <ContentSection title="Upload Results" color="accent">
+      <ContentSection
+        title="Upload Results"
+        color="accent"
+        :is-opened="sectionVisibilityStates['upload']"
+      >
         <!-- Game Tabs for Entering Results -->
         <q-tabs
           active-color="primary"
@@ -40,7 +47,11 @@
 
       <!--      <q-separator />-->
       <template v-if="leagueStatus === 'PLAYING'">
-        <ContentSection title="Results" color="positive">
+        <ContentSection
+          title="Results"
+          color="positive"
+          :is-opened="sectionVisibilityStates['results']"
+        >
           <MyLeagueResults />
         </ContentSection>
       </template>
@@ -48,7 +59,11 @@
 
     <!-- Player Cards Grid -->
 
-    <ContentSection title="Players" color="primary">
+    <ContentSection
+      title="Players"
+      color="primary"
+      :is-opened="sectionVisibilityStates['players']"
+    >
       <div class="row q-col-gutter-md q-pa-md">
         <div
           v-for="member in members"
@@ -63,7 +78,7 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue';
+import { onMounted, Ref, ref, watch, watchEffect } from 'vue';
 import GameSelector from 'components/league/GameSelector.vue';
 import LeagueStatusBar from 'pages/LeagueStatusBar.vue';
 import { useLeagueStore } from 'stores/leagueStore';
@@ -91,7 +106,7 @@ const {
   selectedGamesFetchedEmpty,
   selectedGamesById,
   members,
-  leagueId
+  leagueId,
 } = storeToRefs(league);
 const { updateLeagueData, refreshResultsForGame } = league;
 
@@ -106,20 +121,20 @@ function handleSubmit(selectedGameId: number) {
   refreshResultsForGame(selectedGameId);
 }
 
-setLeadText('Select a game to ban')
+setLeadText('Select a game to ban');
 setActions(
   Object.values(selectedGamesById.value)
     .filter((game) => game.selected_by !== user?.username)
     .map((game) => ({
       name: `${game.game_name}`,
       buttonVariant: 'accent',
-      callback: () => handleBanGame(game.id, game.game_name)
+      callback: () => handleBanGame(game.id, game.game_name),
     }))
-)
+);
 
 watch(isMeBanningGame, () => {
   if (isMeBanningGame.value && leagueId.value && user.username) {
-    setLeadText('Select a game to ban')
+    setLeadText('Select a game to ban');
     setActions(
       Object.values(selectedGamesById.value)
         .filter((game) => game.selected_by !== user?.username)
@@ -127,11 +142,11 @@ watch(isMeBanningGame, () => {
           name: `${game.game_name}`,
           buttonVariant: 'primary',
           callback: () => handleBanGame(game.id, game.game_name),
-          autoReset: false
+          autoReset: false,
         }))
-    )
+    );
   }
-})
+});
 
 const { setDialog } = useDialog();
 const $q = useQuasar();
@@ -147,7 +162,7 @@ function handleBanGame(gameId: number, gameName: string) {
         await banGame({
           leagueId: leagueId.value,
           username: user.username,
-          gameId
+          gameId,
         });
         await updateLeagueData();
         $q.notify({
@@ -159,13 +174,54 @@ function handleBanGame(gameId: number, gameName: string) {
       } finally {
         reset();
       }
-
     },
     () => {
-      console.log('Ban cancelled')
+      console.log('Ban cancelled');
     },
     'Ban'
-  )
+  );
 }
 
+/**
+ * Controls visibility of UI sections based on league status
+ * @type {import('vue').Ref<{
+ *   selection: boolean, // Game selection section visibility
+ *   upload: boolean,    // Match upload section visibility
+ *   results: boolean,   // Results section visibility
+ *   players: boolean    // Players section visibility
+ * }>}
+ */
+const sectionVisibilityStates = ref({
+  selection: true,
+  upload: false,
+  results: false,
+  players: true,
+});
+
+/**
+ * Automatically updates section visibility based on league status changes
+ * - PLAYING: Shows upload and results sections
+ * - DONE: Shows only results section
+ * - BANNING: Hides selection, shows players
+ * - PICKING/REPICKING: Shows selection section
+ */
+watchEffect(() => {
+  switch (leagueStatus.value) {
+    case 'PLAYING':
+      sectionVisibilityStates.value['upload'] = true;
+      sectionVisibilityStates.value['results'] = true;
+      break;
+    case 'DONE':
+      sectionVisibilityStates.value['results'] = true;
+      break;
+    case 'BANNING':
+      sectionVisibilityStates.value['selection'] = false;
+      sectionVisibilityStates.value['players'] = true;
+      break;
+    case 'PICKING':
+    case 'REPICKING':
+      sectionVisibilityStates.value['selection'] = true;
+      break;
+  }
+});
 </script>
