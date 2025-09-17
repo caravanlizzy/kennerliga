12978cccdb075af9f1914eb860ad5566ed16e658
Marc Kittner <m.kittner@kunbus.com>
2025-09-17 16:10:48 +0200

    style: Center MYLEGAUE button in Navbar
    Signed-off-by: Marc Kittner <m.kittner@kunbus.com>

--- frontend/src/components/layout/NavBar.vue
+++ frontend/src/components/layout/NavBar.vue
@@ -1,31 +1,56 @@
 <template>
-  <q-btn flat @click="goHome({name: 'home'})">
+  <q-btn flat @click="goHome({ name: 'home' })">
     <q-chip size="md" color="secondary" text-color="white">
-      <q-icon name="psychology" class="q-mr-md" size="xs" color="white"></q-icon>
+      <q-icon
+        name="psychology"
+        class="q-mr-md"
+        size="xs"
+        color="white"
+      ></q-icon>
       Kennerliga
     </q-chip>
   </q-btn>
-  <q-toolbar-title class="text-primary">
-    <!--      <h6 class="text-italic" > Kennerliga </h6>-->
+  <q-toolbar-title class="text-primary justify-center row no-wrap">
+    <KennerButton
+      :to="{ name: 'my-league' }"
+      v-if="isAuthenticated"
+      class="my-league-btn q-px-md"
+      color="info"
+    >
+      <q-avatar size="24px" class="q-mr-sm">
+        <q-icon name="emoji_events" color="amber-4" />
+      </q-avatar>
+      <div class="text-weight-bold text-primary">My League</div>
+    </KennerButton>
   </q-toolbar-title>
   <KennerButton
     @click="toggleDev"
     color="grey-8"
-    dense
     unelevated
     flat
     icon="build"
-    label="Dev"
     class="q-mt-sm q-ml-sm"
   >
   </KennerButton>
 
-  <KennerButton :to="{name: 'my-league'}" v-if="isAuthenticated" flat color="positive" icon="sports_esports">
-    <KennerTooltip> Meine Liga </KennerTooltip>
-  </KennerButton>
-  <UserName v-if="isAuthenticated"/>
-  <KennerButton color="primary" v-if="isAuthenticated" flat icon="menu" @click="onToggle"/>
-  <KennerButton color="primary" v-else flat icon="login" :to="{name:'login'}"/>
+
+
+
+  <UserName v-if="isAuthenticated" />
+  <KennerButton
+    color="primary"
+    v-if="isAuthenticated"
+    flat
+    icon="menu"
+    @click="onToggle"
+  />
+  <KennerButton
+    color="primary"
+    v-else
+    flat
+    icon="login"
+    :to="{ name: 'login' }"
+  />
 </template>
 
 <script setup lang="ts">
@@ -40,8 +65,8 @@ import { ref } from 'vue';
 import { useUiStore } from 'stores/uiStore';
 
 defineProps<{
-  onToggle: () => void,
-}>()
+  onToggle: () => void;
+}>();
 
 const store = useUserStore();
 const router = useRouter();
@@ -51,5 +76,16 @@ const { isAuthenticated } = storeToRefs(store);
 function goHome() {
   router.push({ name: 'home' });
 }
-
 </script>
+
+<style lang="scss" scoped>
+.my-league-btn {
+  border-radius: 8px;
+  transition: all 0.3s ease;
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  }
+}
+</style>
