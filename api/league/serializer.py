7e30a1e67dcff7bde7ee42e90759a5c779dbc63c
Marc Kittner <marc.kittner@posteo.de>
2025-09-27 14:12:37 +0200

    refactor: delete unused lines in LeaugeDetailSerializer

--- api/league/serializer.py
+++ api/league/serializer.py
@@ -14,36 +14,6 @@ class LeagueSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-# class LeagueMemberSerializer(serializers.ModelSerializer):
-#     username = serializers.CharField(source='profile.user.username', read_only=True)
-#     profile_name = serializers.CharField(source='profile.profile_name', read_only=True)
-#     selected_game = serializers.SerializerMethodField()
-#     game = serializers.SerializerMethodField()
-#     is_active_player = serializers.SerializerMethodField()
-#
-#     class Meta:
-#         model = SeasonParticipant
-#         fields = ['id', 'username', 'profile_name', 'selected_game', 'game', 'is_active_player']
-#
-#     def get_selected_game(self, participant):
-#         selected_game = SelectedGame.objects.filter(
-#             player=participant.profile,
-#             league=self.context['league']
-#         ).first()
-#         return str(selected_game.game) if selected_game else None
-#
-#     def get_banned_game(self, participant):
-#         ban = BanDecision.objects.filter(
-#             player=participant.profile,
-#             league=self.context['league']
-#         ).first()
-#         return str(ban.banned_game.game) if ban and ban.banned_game else None
-#
-#     def get_is_active_player(self, participant):
-#         league = self.context.get('league')
-#         return league.active_player_id == participant.id if league else False
-
-
 class LeagueDetailSerializer(serializers.ModelSerializer):
     members = serializers.SerializerMethodField()
 
@@ -73,10 +43,10 @@ class LeagueDetailSerializer(serializers.ModelSerializer):
         bans_map = defaultdict(list)
         if sg_ids:
             for sg_id, name in (
-                BanDecision.objects
-                .filter(league=league, selected_game_id__in=sg_ids)
-                .select_related('player_banning')
-                .values_list('selected_game_id', 'player_banning__user__username')
+                    BanDecision.objects
+                            .filter(league=league, selected_game_id__in=sg_ids)
+                            .select_related('player_banning')
+                            .values_list('selected_game_id', 'player_banning__user__username')
             ):
                 bans_map[sg_id].append(name)
 
@@ -93,6 +63,3 @@ class LeagueDetailSerializer(serializers.ModelSerializer):
                 member.pop('banned_game', None)  # or keep only an id if you prefer
 
         return data
-
-
-
