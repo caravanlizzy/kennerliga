fa01be10b7512d16d54e40004137bbd244bca590
Marc Kittner <marc.kittner@posteo.de>
2025-09-27 22:37:58 +0200

    style: Imstyle: Update ErrorPage

--- frontend/src/pages/ErrorNotFoundPage.vue
+++ frontend/src/pages/ErrorNotFoundPage.vue
@@ -1,27 +1,47 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
-        404
-      </div>
-
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
-
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
+  <div class="fullscreen bg-grey-1 text-center q-pa-md flex flex-center column">
+    <!-- Error code -->
+    <div class="text-h2 text-accent text-bold">
+      {{ code }}
     </div>
+
+    <!-- Message -->
+    <div class="text-h5 q-mt-md">
+      {{ message }}
+    </div>
+
+    <!-- Extra description -->
+    <div v-if="description" class="text-subtitle1 q-mt-sm text-grey-7">
+      {{ description }}
+    </div>
+
+    <!-- Actions -->
+    <KennerButton
+      class="q-mt-xl"
+      color="positive"
+      icon="home"
+      label="Go Home"
+      to="/"
+      size="lg"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import KennerButton from 'components/base/KennerButton.vue';
 
+defineProps({
+  code: {
+    type: [String, Number],
+    default: 404
+  },
+  message: {
+    type: String,
+    default: 'Oops! Page not found.'
+  },
+  description: {
+    type: String,
+    default: 'The page you are looking for doesn’t exist.'
+  }
+});
 </script>
