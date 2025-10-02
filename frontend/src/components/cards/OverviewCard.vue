eb21946df365969eff7834c81d0c822a959e80b3
Marc Kittner <marc.kittner@posteo.de>
2025-10-02 21:25:07 +0200

    Remove elevation from OverviewCard

--- frontend/src/components/cards/OverviewCard.vue
+++ frontend/src/components/cards/OverviewCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card bordered class="q-mt-lg column q-mx-md">
+  <q-card bordered flat class="q-mt-lg column q-mx-md">
 <!--    <q-card-section class="row justify-between q-pa-md text-h6 text-secondary">-->
 <!--      <slot name="title" />-->
 <!--    </q-card-section>-->
