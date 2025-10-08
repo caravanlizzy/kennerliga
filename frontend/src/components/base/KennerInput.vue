38c4dd37e20582ac3d6ddafb8782384dc9f355ca
Marc Kittner <m.kittner@kunbus.com>
2025-10-08 15:07:14 +0200

    feat: Update KennerInput to pass named slots
    Signed-off-by: Marc Kittner <m.kittner@kunbus.com>

--- frontend/src/components/base/KennerInput.vue
+++ frontend/src/components/base/KennerInput.vue
@@ -1,5 +1,12 @@
 <template>
-  <q-input hide-bottom-space  :dark="dark"  color="primary" v-model="modelValue" :label="label" lazy-rules :rules="rules" />
+  <q-input hide-bottom-space  :dark="dark"  color="primary" v-model="modelValue" :label="label" lazy-rules :rules="rules">
+    <template #prepend><slot name="prepend" /></template>
+    <template #append><slot name="append" /></template>
+    <template #before><slot name="before" /></template>
+    <template #after><slot name="after" /></template>
+    <template #hint><slot name="hint" /></template>
+    <template #counter><slot name="counter" /></template>
+  </q-input>
 </template>
 
 <script setup lang="ts">
