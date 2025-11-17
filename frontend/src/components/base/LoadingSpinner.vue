a7da53fea5e5b4009dc3dbd1e161b3701d67ba92
Marc Kittner <marc.kittner@posteo.de>
2025-11-17 12:21:59 +0100

    style: update loading spinner color

--- frontend/src/components/base/LoadingSpinner.vue
+++ frontend/src/components/base/LoadingSpinner.vue
@@ -3,8 +3,8 @@
     class="q-pa-xl flex flex-center column q-gutter-md"
     style="min-height: 60vh"
   >
-    <q-spinner-ios size="64px" color="accent" />
-    <div class="text-subtitle1 text-accent">
+    <q-spinner-ios size="64px" color="primary" />
+    <div class="text-subtitle1 text-primary">
       <span v-if="text"> {{text}}</span>
       <span v-else> Loading...</span>
     </div>
