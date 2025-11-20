0b2aab25596fc874fb9b1451d1fb1aef2dd01a1d
Marc Kittner <m.kittner@kunbus.com>
2025-11-20 10:15:53 +0100

    feat: Kennerdrawer style

---
+++ frontend/src/components/base/DrawerSubGroup.vue
@@ -0,0 +1,7 @@
+<template>
+  <q-item-label
+    header
+    class="text-grey-7 text-weight-light text-caption q-py-sm"
+    ><slot
+  /></q-item-label>
+</template>
--- frontend/src/components/layout/KennerDrawer.vue
+++ frontend/src/components/layout/KennerDrawer.vue
@@ -8,15 +8,29 @@
       :width="220"
     >
       <q-list>
-        <DrawerItem icon="build" label="Hijack" forward-name="dev"/>
         <DrawerItem icon="feedback" label="Feedback" forward-name="feedback" />
-        <DrawerItem
-          icon="calendar_month"
-          label="Seasons"
-          forward-name="seasons"
-        />
-        <DrawerItem icon="casino" label="Games" forward-name="games" />
-        <DrawerItem icon="group" label="Members" forward-name="users" />
+
+        <q-separator class="q-my-sm" />
+
+        <template v-if="isAdmin">
+          <DrawerSubGroup>Admin</DrawerSubGroup>
+          <DrawerItem icon="casino" label="Games" forward-name="games" />
+          <DrawerItem icon="group" label="Members" forward-name="users" />
+          <DrawerItem
+            icon="calendar_month"
+            label="Seasons"
+            forward-name="seasons"
+          />
+          <q-separator class="q-my-sm" />
+
+        </template>
+
+
+        <DrawerSubGroup>Dev</DrawerSubGroup>
+        <DrawerItem icon="build" label="Hijack" forward-name="dev" />
+
+        <q-separator class="q-my-sm" />
+
         <DrawerItem icon="logout" label="Logout" @click="logout" />
       </q-list>
     </q-drawer>
@@ -28,11 +42,14 @@
 import DrawerItem from 'components/base/DrawerItem.vue';
 import { useUserStore } from 'stores/userStore';
 import { useResponsive } from 'src/composables/reponsive';
+import DrawerSubGroup from 'components/base/DrawerSubGroup.vue';
+import { storeToRefs } from 'pinia';
 
 const drawerState = defineModel();
 
 const responsive = useResponsive();
 const { logout } = useUserStore();
+const { isAdmin } = storeToRefs(useUserStore());
 
 const { isMobile } = responsive;
 </script>
