7e570527ce26a72ebe86a6ab88bf882e1db9df1f
haligh <marc.kittner@posteo.de>
2025-12-07 23:08:03 +0100

    feat: update Announcement styles

--- frontend/src/components/ui/AnnouncementDisplay.vue
+++ frontend/src/components/ui/AnnouncementDisplay.vue
@@ -3,21 +3,30 @@
     <q-banner
       v-for="announcement in announcements"
       :key="announcement.id"
-      :class="bannerClasses[announcement.type]"
+      :class="[bannerClasses[announcement.type], 'q-py-md']"
     >
-      <template #avatar>
-        <q-icon
-          :name="announcementIcons[announcement.type]"
-          size="md"
-        />
-      </template>
+      <div class="full-width flex flex-center">
+        <div
+          class="column items-center text-center"
+          style="max-width:780px;"
+        >
+          <!-- Avatar + title row -->
+          <div class="row items-center justify-center q-gutter-sm">
+            <q-icon
+              :name="announcementIcons[announcement.type]"
+              size="md"
+            />
 
-      <div class="text-subtitle1 text-weight-bold">
-        {{ announcement.title }}
-      </div>
+            <div class="text-subtitle1 text-weight-bold">
+              {{ announcement.title }}
+            </div>
+          </div>
 
-      <div v-if="announcement.content" class="q-mt-xs">
-        {{ announcement.content }}
+          <!-- Optional content -->
+          <div v-if="announcement.content" class="q-mt-sm">
+            {{ announcement.content }}
+          </div>
+        </div>
       </div>
     </q-banner>
   </div>
