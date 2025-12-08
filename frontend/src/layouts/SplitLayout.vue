2a828a15783c5a0606b969edf528d9de6423140a
haligh <marc.kittner@posteo.de>
2025-12-08 16:34:19 +0100

    feat: Add Split layout

---
+++ frontend/src/layouts/SplitLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row full-width full-height">
+    <!-- Left -->
+    <div class="col-12 col-md-6 q-pr-sm q-mb-sm q-mb-md-0">
+      <slot name="left">
+        <slot />
+      </slot>
+    </div>
+
+    <!-- Right -->
+    <div class="col-12 col-md-6 q-pl-sm">
+      <slot name="right" />
+    </div>
+  </div>
+</template>
