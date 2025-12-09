d9b238a06269bb41242e84475ba4f1c13396fba8
haligh <marc.kittner@posteo.de>
2025-12-09 13:57:32 +0100

    feat Remove fetchAnnouncements from export

--- frontend/src/stores/announcementStore.ts
+++ frontend/src/stores/announcementStore.ts
@@ -76,7 +76,6 @@ export const useAnnouncementStore = defineStore('announcement', () => {
     announcementIcons,
 
     // actions
-    fetchAnnouncements,
     addAnnouncement,
     removeAnnouncement,
   };
