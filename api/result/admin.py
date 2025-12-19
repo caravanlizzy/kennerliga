683c938b4223e366a46936bd4c04d898d8ac83e4
Marc Kittner <m.kittner@kunbus.com>
2025-12-19 11:13:29 +0100

    feat: Update Faction admin
    Signed-off-by: Marc Kittner <m.kittner@kunbus.com>

--- api/result/admin.py
+++ api/result/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-
 from result.models import Result
 
 
@@ -8,6 +7,7 @@ class ResultAdmin(admin.ModelAdmin):
     list_display = (
         'player_profile',
         'get_selected_game',
+        'get_factions',
         'league',
         'season',
         'points',
@@ -28,7 +28,28 @@ class ResultAdmin(admin.ModelAdmin):
 
     readonly_fields = ('tie_breaker_resolved',)
 
+    # Optimizes Foreign Key lookups in the list view (prevents N+1 queries)
+    list_select_related = (
+        'player_profile',
+        'league',
+        'season',
+        'selected_game',
+        'selected_game__game'
+    )
+
+    def get_queryset(self, request):
+        """
+        Optimizes Many-to-Many lookups for the factions list.
+        """
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related('factions')
+
     def get_selected_game(self, obj):
         return str(obj.selected_game)
 
     get_selected_game.short_description = 'Selected Game'
+
+    def get_factions(self, obj):
+        return ", ".join([f.name for f in obj.factions.all()])
+
+    get_factions.short_description = 'Factions'
