f268d4d14d5ece6f5ea0cc61e7aa269e1c61ac64
haligh <marc.kittner@posteo.de>
2025-12-30 13:51:41 +0100

    feat: apply production settings for nightly

--- scripts/nightly_tasks.sh
+++ scripts/nightly_tasks.sh
@@ -2,16 +2,16 @@
 set -e
 
 # Path to your project and virtualenv
-PROJECT_DIR="/home/haligh/projects/kennerliga/api"
-VENV_PYTHON="/home/haligh/projects/kennerliga/api/.venv/bin/python"
+PROJECT_DIR="/home/haligh/kennerliga/api"
+VENV_PYTHON="/home/haligh/.virtualenvs/kennerliga-venv/bin/python"
 
 # 1) Run the backup command
 export DJANGO_SETTINGS_MODULE="django_rest.settings_production"
-"$VENV_PYTHON" "$PROJECT_DIR/manage.py" db_backup
+"$VENV_PYTHON" "$PROJECT_DIR/prod_manage.py" db_backup
 
 # 2) Run season start if last day of the month
 TOMORROW_DAY=$(date -d tomorrow +%d)
 if [ "$TOMORROW_DAY" = "01" ]; then
     echo "Last day of the month detected, starting new season..."
-    "$VENV_PYTHON" "$PROJECT_DIR/manage.py" start_new_season
+    "$VENV_PYTHON" "$PROJECT_DIR/prod_manage.py" start_new_season
 fi
