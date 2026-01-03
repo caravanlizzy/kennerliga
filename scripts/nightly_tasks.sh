5fc6e44a046bbbd80c817519340185a5304da22e
haligh <marc.kittner@posteo.de>
2026-01-03 15:10:45 +0100

    feat: remove season creation from script

--- scripts/nightly_tasks.sh
+++ scripts/nightly_tasks.sh
@@ -10,8 +10,8 @@ export DJANGO_SETTINGS_MODULE="django_rest.settings_production"
 "$VENV_PYTHON" "$PROJECT_DIR/prod_manage.py" db_backup
 
 # 2) Run season start if last day of the month
-TOMORROW_DAY=$(date -d tomorrow +%d)
-if [ "$TOMORROW_DAY" = "01" ]; then
-    echo "Last day of the month detected, starting new season..."
-    "$VENV_PYTHON" "$PROJECT_DIR/prod_manage.py" start_new_season
-fi
+#TOMORROW_DAY=$(date -d tomorrow +%d)
+#if [ "$TOMORROW_DAY" = "01" ]; then
+#    echo "Last day of the month detected, starting new season..."
+#    "$VENV_PYTHON" "$PROJECT_DIR/prod_manage.py" start_new_season
+#fi
