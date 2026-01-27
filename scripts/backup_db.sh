931c6545a3f5064f4d0b4e0d99714f27abe76663
haligh <marc.kittner@posteo.de>
2026-01-27 15:26:19 +0100

    feat(scripts): rename nightly_tasks.sh to backup_db.sh and focus on backups

--- scripts/backup_db.sh
+++ scripts/backup_db.sh
@@ -8,10 +8,3 @@ VENV_PYTHON="/home/haligh/.virtualenvs/kennerliga-venv/bin/python"
 # 1) Run the backup command
 export DJANGO_SETTINGS_MODULE="django_rest.settings_production"
 "$VENV_PYTHON" "$PROJECT_DIR/prod_manage.py" db_backup
-
-# 2) Run season start if last day of the month
-#TOMORROW_DAY=$(date -d tomorrow +%d)
-#if [ "$TOMORROW_DAY" = "01" ]; then
-#    echo "Last day of the month detected, starting new season..."
-#    "$VENV_PYTHON" "$PROJECT_DIR/prod_manage.py" start_new_season
-#fi
