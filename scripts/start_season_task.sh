666a857071a571084a2b38fa4f5f54145cfcdc4c
haligh <marc.kittner@posteo.de>
2026-01-28 22:37:55 +0100

    feat(scripts): run start_new_season only on the last day of the month

--- scripts/start_season_task.sh
+++ scripts/start_season_task.sh
@@ -5,6 +5,10 @@ set -e
 PROJECT_DIR="/home/haligh/kennerliga/api"
 VENV_PYTHON="/home/haligh/.virtualenvs/kennerliga-venv/bin/python"
 
-# Run the start new season command
-export DJANGO_SETTINGS_MODULE="django_rest.settings_production"
-"$VENV_PYTHON" "$PROJECT_DIR/prod_manage.py" start_new_season
+# Run the start new season command only on the last day of the month
+if [ "$(date -d "+1 day" +%d)" = "01" ]; then
+    export DJANGO_SETTINGS_MODULE="django_rest.settings_production"
+    "$VENV_PYTHON" "$PROJECT_DIR/prod_manage.py" start_new_season
+else
+    echo "Today is not the last day of the month. Skipping season creation."
+fi
