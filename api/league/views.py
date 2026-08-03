62c7beff8d6b39e2b42cc11bb915e1f1fad538d3
haligh <marc.kittner@posteo.de>
2026-08-03 14:36:23 +0200

    fix: Add missing import Response

--- api/league/views.py
+++ api/league/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
 from api.permissions import IsAdminOrReadOnly
 
